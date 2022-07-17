bd735e3e89dda0fd7f20c2543014344aa675b8da
Maria Eduarda Z. Pereira <duda.zanin66@gmail.com>
2022-07-17 10:21:27 -0300

    implementação relatórios

--- src/main/java/br/edu/ifsul/controle/ControleCidade.java
+++ src/main/java/br/edu/ifsul/controle/ControleCidade.java
@@ -1,63 +1,84 @@
 package br.edu.ifsul.controle;
 
-
 import br.edu.ifsul.dao.CidadeDAO;
 import br.edu.ifsul.modelo.Cidade;
 import br.edu.ifsul.util.Util;
+import br.edu.ifsul.util.UtilRelatorios;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 @Named(value = "controleCidade")
 @ViewScoped
-public class ControleCidade implements Serializable{
+public class ControleCidade implements Serializable {
+
     @EJB
     private CidadeDAO<Cidade> dao;
     private Cidade objeto;
 
-    public ControleCidade(){
-        
+    public ControleCidade() {
+
     }
-    
-    public String listar(){
-       return "/privado/cidade/listar?faces-redirect=true"; 
+
+    public void imprimeCidade() {
+        HashMap parametros = new HashMap();
+        UtilRelatorios.imprimeRelatorio("relatorioCidades", parametros, dao.getListaTodos());
     }
-    
-    public void novo(){
+
+    public void imprimeCidade(Object id) {
+        try {
+            objeto = dao.getObjectByID(id);
+            List<Cidade> lista = new ArrayList<>();
+            lista.add(objeto);
+            HashMap parametros = new HashMap();
+            UtilRelatorios.imprimeRelatorio("relatorioCidades", parametros, lista);
+        } catch (Exception e) {
+            Util.mensagemInformacao("Erro ao imprimir: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public String listar() {
+        return "/privado/cidade/listar?faces-redirect=true";
+    }
+
+    public void novo() {
         setObjeto(new Cidade());
     }
-    
-    public void alterar(Object id){
-        try{
+
+    public void alterar(Object id) {
+        try {
             setObjeto(getDao().getObjectByID(id));
-        }catch (Exception e){
+        } catch (Exception e) {
             Util.mensagemInformacao("Erro ao recuperar objeto: " + Util.getMensagemErro(e));
         }
     }
-    
-     public void excluir(Object id){
-        try{
+
+    public void excluir(Object id) {
+        try {
             setObjeto(getDao().getObjectByID(id));
             getDao().remove(getObjeto());
             Util.mensagemInformacao("Objeto removido com sucesso!");
-        }catch (Exception e){
+        } catch (Exception e) {
             Util.mensagemInformacao("Erro ao recuperar objeto: " + Util.getMensagemErro(e));
         }
     }
-     
-     public void salvar(){
-         try{
-             if(getObjeto().getId() == null){
-                 getDao().persist(getObjeto());
-             }else{
-                 getDao().merge(getObjeto());
-             }
-             Util.mensagemInformacao("Objeto persistido com sucesso!");
-         }catch (Exception e){
+
+    public void salvar() {
+        try {
+            if (getObjeto().getId() == null) {
+                getDao().persist(getObjeto());
+            } else {
+                getDao().merge(getObjeto());
+            }
+            Util.mensagemInformacao("Objeto persistido com sucesso!");
+        } catch (Exception e) {
             Util.mensagemInformacao("Erro ao salvar objeto: " + Util.getMensagemErro(e));
         }
-     }
+    }
 
     public CidadeDAO<Cidade> getDao() {
         return dao;
@@ -74,5 +95,5 @@ public class ControleCidade implements Serializable{
     public void setObjeto(Cidade objeto) {
         this.objeto = objeto;
     }
-     
+
 }
